4e3e8880ff86ba38813c69e61c1d4aff018e33f6
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-07-30 16:23:25 +0200

    Curso de Python

---
+++ dia4/control_de_flujo.py
@@ -0,0 +1,11 @@
+# Los controladores de flujo son if, elif y else
+
+a = 10
+b = 5
+
+if a > b:
+    print("a es mayor que b")
+elif a < b:
+    print("a es menor que b")
+else:
+    print("a es igual que b")
