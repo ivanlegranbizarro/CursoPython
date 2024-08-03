493eba2f4ff04bddbbc0bba23f5abfbfd7e27cd4
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-03 09:04:33 +0200

    control_de_flujo

--- dia4/control_de_flujo.py
+++ dia4/control_de_flujo.py
@@ -9,3 +9,15 @@ elif a < b:
     print("a es menor que b")
 else:
     print("a es igual que b")
+
+mascota = "tortuga"
+
+match mascota:
+    case "perro":
+        print("Tienes un perro")
+    case "gato":
+        print("Tienes un gato")
+    case "tortuga":
+        print("Tienes una tortuga")
+    case _:
+        print("No sé qué mascota tienes")
