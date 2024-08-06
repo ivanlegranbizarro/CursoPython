f79e709b54043ed7f267b8ae5bc22a6999226172
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-06 20:28:46 +0200

    devolver una tupla

---
+++ dia5/ejemplo_funcion.py
@@ -0,0 +1,22 @@
+precios_cafe = [("capuccino", 1.20), ("latte", 1.00), ("americano", 0.80)]
+
+
+for precio in precios_cafe:
+    print(f"El precio del cafe {precio[0]} es {precio[1]}")
+
+
+def cafe_mas_caro(lista_precios):
+    precio_mayor = 0
+    cafe_mas_caro = ""
+
+    for cafe, precio in lista_precios:
+        if precio_mayor < precio:
+            precio_mayor = precio
+            cafe_mas_caro = cafe
+
+    return (cafe_mas_caro, precio_mayor)
+
+
+cafe, precio = cafe_mas_caro(precios_cafe)
+
+print(f"El cafe más caro es {cafe} y cuesta {precio}€")
