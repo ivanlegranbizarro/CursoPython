23927cad9aa2ab8f69c43af6f051d52f6e72e7b2
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-09 14:53:26 +0200

    seteo del proyecto

---
+++ dia6/Proyecto6.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from os import system
+
+# 1 - leer receta: elegir categoría, mostrar recetas, mostrar receta elegida
+# 2 - crear receta: elegir categoría, escribir nombre nueva receta, crear contenido
+# 3 - crear categoría: escribir nombre nueva categoría (crear carpeta con ese nombre)
+# 4 - eliminar receta (como la 1 pero en lugar de mostrar la receta, la va a borrar)
+# 5 - eliminar categoría: como la 3 pero en lugar de crearla, la va a borrar
+# 6 - finalizar programa: salir del bucle principal
+
+# Sería bueno que cada vez que el usuario sala del programa se limpie la pantalla
+
+# Funciones
