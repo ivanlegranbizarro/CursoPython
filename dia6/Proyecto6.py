bd9168f5c1e9ee8034c4a0aa478031abea15a2b9
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-09 21:10:10 +0200

    Paso 2 hecho

--- dia6/Proyecto6.py
+++ dia6/Proyecto6.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 from os import system
+from pathlib import Path
 
 # 1 - leer receta: elegir categoría, mostrar recetas, mostrar receta elegida
 # 2 - crear receta: elegir categoría, escribir nombre nueva receta, crear contenido
@@ -45,6 +45,34 @@ def mostrar_listado_recetas_de_categoria_escogida(ruta_categoria_escogida):
         print(i, receta.name)
 
 
-ruta_cateogira_escogida = elegir_categoria(ruta_categorias)
+def escoger_receta(ruta_categoria_escogida):
+    mostrar_listado_recetas_de_categoria_escogida(ruta_categoria_escogida)
+    listado_recetas = list(ruta_categoria_escogida.iterdir())
+    while True:
+        try:
+            seleccion = int(input("Elige una receta: "))
+            if seleccion < 1 or seleccion > len(listado_recetas):
+                print("La receta no existe")
+            else:
+                return listado_recetas[seleccion - 1]
+        except ValueError:
+            print("Por favor, introduce un número")
 
-mostrar_listado_recetas_de_categoria_escogida(ruta_cateogira_escogida)
+
+def abrir_receta_escogida(receta_escogida):
+    with open(receta_escogida) as f:
+        print(f.read())
+
+
+def crear_nueva_receta(ruta_categoria_escogida):
+    escoger_nombre_nueva_receta = input("Introduce el nombre de la nueva receta: ")
+    if Path(ruta_categoria_escogida / escoger_nombre_nueva_receta).exists():
+        print("La receta ya existe")
+    else:
+        with open(ruta_categoria_escogida / escoger_nombre_nueva_receta, "w") as f:
+            f.write(input("Introduce el contenido de la nueva receta: "))
+
+
+ruta_categoria_escogida = elegir_categoria(ruta_categorias)
+
+crear_nueva_receta(ruta_categoria_escogida)
