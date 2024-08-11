038ec5d8d2fa4447629b5b6578c712c90daf9927
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-11 11:11:24 +0200

    estructura inicial con match

--- dia6/Proyecto6.py
+++ dia6/Proyecto6.py
@@ -1,78 +1,46 @@
 from os import system
 from pathlib import Path
 
-# 1 - leer receta: elegir categoría, mostrar recetas, mostrar receta elegida
-# 2 - crear receta: elegir categoría, escribir nombre nueva receta, crear contenido
-# 3 - crear categoría: escribir nombre nueva categoría (crear carpeta con ese nombre)
-# 4 - eliminar receta (como la 1 pero en lugar de mostrar la receta, la va a borrar)
-# 5 - eliminar categoría: como la 3 pero en lugar de crearla, la va a borrar
-# 6 - finalizar programa: salir del bucle principal
+# Mostrar menú de inicio
 
-# Sería bueno que cada vez que el usuario sala del programa se limpie la pantalla
+menu = 0
 
-# Funciones
-
-
-ruta_categorias = Path().home() / "Recetas"
-
-
-def mostrar_categorias(ruta):
-    print("Las categorías son:")
-    listado_categorias = list(ruta.iterdir())
-    for i, categoria in enumerate(listado_categorias, start=1):
-        print(i, categoria.name)
-    return listado_categorias
-
-
-def elegir_categoria(ruta):
-    listado_categorias = mostrar_categorias(ruta)
-
-    while True:
-        try:
-            seleccion = int(input("Elige una categoría: "))
-            if seleccion < 1 or seleccion > len(listado_categorias):
-                print("La categoría no existe")
-            else:
-                return listado_categorias[seleccion - 1]
-        except ValueError:
-            print("Por favor, introduce un número")
-
-
-def mostrar_listado_recetas_de_categoria_escogida(ruta_categoria_escogida):
-    print(f"Las recetas de {ruta_categoria_escogida.name} son:")
-    listado_recetas = list(ruta_categoria_escogida.iterdir())
-    for i, receta in enumerate(listado_recetas, start=1):
-        print(i, receta.name)
-
-
-def escoger_receta(ruta_categoria_escogida):
-    mostrar_listado_recetas_de_categoria_escogida(ruta_categoria_escogida)
-    listado_recetas = list(ruta_categoria_escogida.iterdir())
-    while True:
-        try:
-            seleccion = int(input("Elige una receta: "))
-            if seleccion < 1 or seleccion > len(listado_recetas):
-                print("La receta no existe")
-            else:
-                return listado_recetas[seleccion - 1]
-        except ValueError:
-            print("Por favor, introduce un número")
-
-
-def abrir_receta_escogida(receta_escogida):
-    with open(receta_escogida) as f:
-        print(f.read())
-
-
-def crear_nueva_receta(ruta_categoria_escogida):
-    escoger_nombre_nueva_receta = input("Introduce el nombre de la nueva receta: ")
-    if Path(ruta_categoria_escogida / escoger_nombre_nueva_receta).exists():
-        print("La receta ya existe")
-    else:
-        with open(ruta_categoria_escogida / escoger_nombre_nueva_receta, "w") as f:
-            f.write(input("Introduce el contenido de la nueva receta: "))
-
-
-ruta_categoria_escogida = elegir_categoria(ruta_categorias)
-
-crear_nueva_receta(ruta_categoria_escogida)
+match menu:
+    case 1:
+        # mostrar todas las categorías
+        # elegir categoría
+        # mostrar recetas de esa categoría
+        # elegir una de esas recetas
+        # mostrar esa receta
+        # volver al inicio
+        pass
+    case 2:
+        # mostrar categorías
+        # elegir categoría
+        # crear receta nueva
+        # volver al inicio
+        pass
+    case 3:
+        # crear categoría nueva
+        # volver al inicio
+        pass
+    case 4:
+        # mostrar todas las categorías
+        # elegir categoría
+        # mostrar recetas de esa categoría
+        # elegir una de esas recetas
+        # eliminar esa receta
+        # volver al inicio
+        pass
+    case 5:
+        # mostrar todas las categorías
+        # elegir una categoría
+        # eliminar la categoría
+        # volver al inicio
+        pass
+    case 6:
+        # salir del programa
+        pass
+    case _:
+        # manejar caso en el que `menu` no coincide con ningún valor esperado
+        pass
