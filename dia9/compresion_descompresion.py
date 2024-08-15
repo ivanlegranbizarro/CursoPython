4544112824f083966078cdf518b6554c2d8cf4b1
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-15 09:44:05 +0200

    eso

--- dia9/compresion_descompresion.py
+++ dia9/compresion_descompresion.py
@@ -3,9 +3,9 @@
 import zipfile
 
 # Comprimir
-# with zipfile.ZipFile("archivo_comprimido.zip", "w") as mi_zip:
-#     mi_zip.write("mi_texto_A.txt", compress_type=zipfile.ZIP_DEFLATED)
-#     mi_zip.write("mi_texto_B.txt", compress_type=zipfile.ZIP_DEFLATED)
+with zipfile.ZipFile("archivo_comprimido.zip", "w") as mi_zip:
+    mi_zip.write("mi_texto_A.txt", compress_type=zipfile.ZIP_DEFLATED)
+    mi_zip.write("mi_texto_B.txt", compress_type=zipfile.ZIP_DEFLATED)
 
 # Descomprimir
 with zipfile.ZipFile("archivo_comprimido.zip", "r") as mi_zip:
