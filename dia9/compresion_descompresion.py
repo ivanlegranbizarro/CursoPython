aeaa55f997fc39a7e3832c9890f4b088b4663b1e
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-15 09:41:50 +0200

    comprimir y descomprimir archivos

---
+++ dia9/compresion_descompresion.py
@@ -0,0 +1,12 @@
+# Zipfile y shutil sirven para comprimir y descomprimir archivos
+
+import zipfile
+
+# Comprimir
+# with zipfile.ZipFile("archivo_comprimido.zip", "w") as mi_zip:
+#     mi_zip.write("mi_texto_A.txt", compress_type=zipfile.ZIP_DEFLATED)
+#     mi_zip.write("mi_texto_B.txt", compress_type=zipfile.ZIP_DEFLATED)
+
+# Descomprimir
+with zipfile.ZipFile("archivo_comprimido.zip", "r") as mi_zip:
+    mi_zip.extractall()
