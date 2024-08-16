7bc58eadbdfa2d15b1f959a78b36fcb7ff463a9f
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-16 09:41:42 +0200

    movimiento nave

--- dia10/main.py
+++ dia10/main.py
@@ -11,10 +11,12 @@ ejecutandose = True
 nave_jugador = pygame.image.load("spaceship.png")
 x_jugador = 368
 y_jugador = 536
+x_cambio_jugador = 0
+y_cambio_jugador = 0
 
 
-def jugador():
-    pantalla.blit(nave_jugador, (x_jugador, y_jugador))
+def jugador(x, y):
+    pantalla.blit(nave_jugador, (x, y))
 
 
 # Título e icono
@@ -24,13 +26,24 @@ pygame.display.set_icon(icono_ovni)
 
 # Loop de ejecución del juego
 while ejecutandose:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            ejecutandose = False
 
     # Color RGB de la pantalla del juego
     pantalla.fill((205, 144, 228))
 
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            ejecutandose = False
+
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                x_cambio_jugador -= 0.1
+            if evento.key == pygame.K_RIGHT:
+                x_cambio_jugador += 0.1
+        if evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
+                x_cambio_jugador = 0
+
     # Invocamos la aparición de la nave del jugador
-    jugador()
+    x_jugador += x_cambio_jugador
+    jugador(x_jugador, y_jugador)
     pygame.display.update()
