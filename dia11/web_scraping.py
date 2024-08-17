9b3713a95926d450228d8028f09b06365fe72052
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-17 09:27:52 +0200

    obtener titulo de la pagina

--- dia11/web_scraping.py
+++ dia11/web_scraping.py
@@ -2,3 +2,9 @@ import bs4
 import requests
 
 url = "https://escueladirecta-blog.blogspot.com/"
+
+resultado = requests.get(url)
+
+sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
+
+print(sopa.select('title')[0].get_text())
