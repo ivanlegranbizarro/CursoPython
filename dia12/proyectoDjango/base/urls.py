b5ac55a46a454fd647a1feff7908f1e93d4c60db
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-20 09:31:07 +0200

    Login

--- dia12/proyectoDjango/base/urls.py
+++ dia12/proyectoDjango/base/urls.py
@@ -6,7 +6,11 @@ app_name = "base"
 
 urlpatterns = [
     path("", views.ListaTareasPendientes.as_view(), name="tareas-pendientes"),
+    path("login/", views.Logueo.as_view(), name="login"),
     path("tarea/<int:pk>/", views.DetalleTarea.as_view(), name="tarea-detalle"),
+    path(
+        "tarea/<int:pk>/eliminar", views.EliminarTarea.as_view(), name="tarea-eliminar"
+    ),
     path("tarea/<int:pk>/editar", views.EditarTarea.as_view(), name="tarea-editar"),
     path("tarea/nueva/", views.CrearTarea.as_view(), name="tarea-nueva"),
 ]
--- dia12/proyectoDjango/base/views.py
+++ dia12/proyectoDjango/base/views.py
@@ -1,8 +1,18 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from .models import Tarea
+
+
+class Logueo(LoginView):
+    template_name = "base/login.html"
+    field = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("base:tareas-pendientes")
 
 
 class ListaTareasPendientes(ListView):
@@ -24,4 +34,9 @@ class CrearTarea(CreateView):
 class EditarTarea(UpdateView):
     model = Tarea
     fields = "__all__"
+    success_url = reverse_lazy("base:tareas-pendientes")
+
+
+class EliminarTarea(DeleteView):
+    model = Tarea
     success_url = reverse_lazy("base:tareas-pendientes")
