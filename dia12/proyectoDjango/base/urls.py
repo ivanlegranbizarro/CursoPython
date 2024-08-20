30848995300e44eabdfd0f2652a1cfb1a654b11c
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-20 09:49:15 +0200

    Logout

--- dia12/proyectoDjango/base/urls.py
+++ dia12/proyectoDjango/base/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from base import views
+from django.contrib.auth.views import LogoutView
 
 app_name = "base"
 
 urlpatterns = [
     path("", views.ListaTareasPendientes.as_view(), name="tareas-pendientes"),
     path("login/", views.Logueo.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="base:login"), name="logout"),
     path("tarea/<int:pk>/", views.DetalleTarea.as_view(), name="tarea-detalle"),
     path(
         "tarea/<int:pk>/eliminar", views.EliminarTarea.as_view(), name="tarea-eliminar"
